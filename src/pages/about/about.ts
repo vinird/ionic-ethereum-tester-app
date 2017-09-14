003d92614ed9855cc1f69177f0e15da5907da339
Michael Vinicio <mvrd_17@hotmail.com>
2017-09-13 23:50:10 -0600

    Redefine dapp

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+
+declare var web3; 
 
 @Component({
   selector: 'page-about',
@@ -7,8 +10,28 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+	private balance : string;
+	public balance_input : any;
+	private loader : any;
 
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) 
+  {
+  	this.balance = "";
+  }
+
+  get_balance()
+  {
+  	this.loader = this.loadingCtrl.create({
+        content: "Please wait...", 
+      });
+  	this.loader.present().then(() => {
+	  	try{
+	  		this .balance = "Eth: " + web3.eth.getBalance(this.balance_input)	
+	  	} catch(err) {
+	  		this .balance = "Invalid account"
+	  	}
+	  	this.loader.dismiss();
+	});
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,97 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { LoadingController } from 'ionic-angular';
 
-@Component({
+// import { BigNumber } from 'bignumber.js';
+declare var web3; 
+// declare var Web3; 
+
+
+
+@Component({ 
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+    // private host = "http://159.203.0.218:8545";
+    private data : string;
+    private loader : any;
+    private accounts : string;
 
-  constructor(public navCtrl: NavController, , private iab: InAppBrowser) {
-  		const browser = this.iab.create('http://159.203.0.218/eth/ethereum_client/');
-  }
+    constructor(public navCtrl: NavController, private iab: InAppBrowser, public loadingCtrl: LoadingController) 
+    {
+       this.data = "No data..";
+       this.accounts = "No data..";
+    }
+
+    ionViewDidLoad()
+    {
+  	}
+
+  	openEthWebClient()
+  	{
+  		this.iab.create('http://159.203.0.218/eth/ethereum_client/');
+  	}
+
+
+    charge_data() {
+      this.loader = this.loadingCtrl.create({
+        content: "Please wait...", 
+      });
+      this.loader.present().then(() => {
+        this.data = "";
+        this.data += "<strong>Estado: </strong> conectado <br/>" 
+         // start web3 filters, calls, etc
+
+        this.data += "<strong>Network: </strong> "+web3.version.network+" <br/>";
+        this.data += "<strong>Ethereum version: </strong> "+web3.version.ethereum+" <br/>";
+        this.data += "<strong>Is listenig: </strong> "+web3.net.listening+" <br/>";
+        this.data += "<strong>Connectes peers: </strong> "+web3.net.peerCount+" <br/>";
+        this.data += "<strong>Ethereum methods: </strong> (check console) F12 <br/>";
+        this.data += "<strong>Ethereum default account: </strong> "+web3.eth.defaultAccount+" <br/>"
+        this.data += "<strong>Ethereum default block: </strong> "+web3.eth.defaultBlock+" <br/>";
+        this.data += "<strong>Ethereum sync: </strong> "+web3.eth.syncing+" <br/>";
+        this.data += "<strong>Ethereum coinbase: </strong> "+web3.eth.coinbase+" <br/>";
+        this.data += "<strong>Ethereum is mining: </strong> "+web3.eth.mining+" <br/>";
+        this.data += "<strong>Ethereum gas price: </strong> "+web3.eth.gasPrice+" <br/>";
+        this.loader.dismiss();
+      })
+   
+
+
+      // console.log("Current block")
+      // console.log(web3.eth.getBlock(web3.eth.blockNumber))
+
+      // console.log("Ethereum methods")
+      // console.log((web3.eth))
+    }
+
+    charge_account(){
+      this.accounts = "";
+      this.loader = this.loadingCtrl.create({
+        content: "Please wait...", 
+      });
+      this.loader.present().then(() => {
+        let account =  web3.eth.accounts;
+        this.accounts += "<strong>Ethereum accounts: </strong>  <br/>";
+        for(var i = 0; i<account.length;i++) { 
+            this.accounts += " "+ account[i] + "<br/>"
+        } 
+        this.loader.dismiss();
+      })
+    }
+
+    presentLoading(state) {
+      if(state) {
+        this.loader = this.loadingCtrl.create({
+          content: "Please wait...", 
+        });
+        this.loader.present();
+      } else {
+        this.loader.dismiss();
+
+      }
+    }
 
 }
