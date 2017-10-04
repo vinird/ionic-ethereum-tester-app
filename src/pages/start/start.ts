20068c0ba72658b22674a0a3e18b2e863fca3085
ChrisMcNulty91 <christopher.mcnulty891@gmail.com>
2017-10-04 21:31:53 +0100

    Saving host url in localstorage

--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the StartPage page.
@@ -14,8 +15,9 @@ import { AlertController } from 'ionic-angular';
   templateUrl: 'start.html',
 })
 export class StartPage {
+  public hostUrl: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
@@ -30,6 +32,16 @@ export class StartPage {
       buttons: ['OK']
     });
     alert.present();
+  }
+
+  saveHostUrl(hostUrl: string) {
+    localStorage.setItem('host-url', hostUrl);
+    const toast = this.toastCtrl.create({
+      message: `Host Url: ${hostUrl} saved successfully`,
+      duration: 4000,
+      position: 'bottom'
+    });
+    toast.present();
   }
 
 }
